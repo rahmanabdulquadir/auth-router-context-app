6e68a335533c2d8afe8d10846c3598f3dc943b49
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-03-07 15:19:48 +0600

    project updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-primary text-primary-content">
-      <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <div className="navbar bg-primary text-primary-content flex justify-evenly">
+      <a className="btn btn-ghost normal-case text-3xl">Awesome - Auth</a>
 
       <div className="flex justify-between">
-        <Link to={"/home"}>Home</Link>
-        <Link to={"/login"}>Log In</Link>
-        <Link to={"/register"}>Register</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to={"/home"}>Home</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to={"/login"}>Log In</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to={"/register"}>Register</Link>
       </div>
     </div>
   );
